95f4fa65caed44c357c0862871140069b4897394
caleb <nunuwillump0857@gmail.com>
2025-05-19 23:12:48 +0800

    website create

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, Flaskbook!"
+
+@app.route("/hello",
+      methods=["GET"],
+      endpoint="hello-endpoint")
+def hello():
+    return "Hello, World!"
