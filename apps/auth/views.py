1854266948763f43f834bf41e858d19c54ce5067
caleb <nunuwillump0857@gmail.com>
2025-06-06 19:09:30 +0800

    auth logout

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -4,7 +4,7 @@ from apps.app import db
 from apps.auth.forms import SignUpForm, LoginForm
 from apps.crud.models import User
 from flask import Blueprint, render_template, flash, url_for, redirect, request
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 
 auth = Blueprint(
@@ -59,3 +59,9 @@ def login():
         
         flash("郵件位址或密碼不正確")
     return render_template("auth/login.html", form=form)
+
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
