e31d95fd1b98bfb50df9728797ebb84ea628983e
caleb <nunuwillump0857@gmail.com>
2025-06-06 23:32:02 +0800

    no template on web

--- apps/app.py
+++ apps/app.py
@@ -27,14 +27,20 @@ def create_app(config_key):
     Migrate(app, db)
     csrf.init_app(app)
 
+    
+
     from apps.crud import views as crud_views
-    from apps.auth import views as auth_views
-
-    from apps.detector import views as dt_views
-
-    app.register_blueprint(dt_views.dt)
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    from apps.auth import views as auth_views
+
     app.register_blueprint(auth_views.auth, url_prefix="/auth")
+    
+    from apps.detector import views as dt_views
+    
+    app.register_blueprint(dt_views.dt)
+    
+    
     return app
 
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -40,7 +40,7 @@ def signup():
 
         next_ = request.args.get("next")
         if next_ is None or not next_.startswith("/"):
-            next_ = url_for("crud.users")
+            next_ = url_for("detector.index")
         return redirect(next_)
 
     return render_template("auth/signup.html", form=form)
@@ -55,7 +55,7 @@ def login():
 
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
         
         flash("郵件位址或密碼不正確")
     return render_template("auth/login.html", form=form)
